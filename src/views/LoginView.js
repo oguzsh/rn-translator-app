2ef29bab02ee7ab4b87e7f0209680f17406930d6
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-04-18 21:54:02 +0300

    edited LoginView

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,22 +1,34 @@
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Alert,
-  ActivityIndicator,
-  SafeAreaView,
-} from 'react-native';
+import {Alert, ActivityIndicator, SafeAreaView} from 'react-native';
 
-import Button from '../components/Button';
+import ActionButton, {ActionButtonTitle} from '../components/ActionButton';
+import Container from '../components/Container';
+import ContainerCenter from '../components/ContainerCenter';
+import Text from '../components/Text';
+import Input from '../components/Input';
+import PasswordInput from '../components/PasswordInput';
+import LoginLogo from '../components/LoginLogo';
 
 import firebase from '../database/firebaseDb';
+
+import theme from '../utils/theme';
 
 function LoginView({navigation}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setLoading] = useState(false);
+  const [passwordToggle, setPasswordToggle] = useState(true);
+  const [icon, setIcon] = useState('eye');
+
+  const togglePassword = () => {
+    if (passwordToggle === false) {
+      setPasswordToggle(true);
+      setIcon('eye');
+    } else {
+      setPasswordToggle(false);
+      setIcon('eye-off');
+    }
+  };
 
   const userLogin = () => {
     if (email === '' && password === '') {
@@ -27,8 +39,8 @@ function LoginView({navigation}) {
         .auth()
         .signInWithEmailAndPassword(email, password)
         .then((res) => {
-          console.log(res);
-          console.log('User logged-in successfully!');
+          //console.log(res);
+          // console.log('User logged-in successfully!');
           setLoading(false);
           setEmail('');
           setPassword('');
@@ -40,67 +52,74 @@ function LoginView({navigation}) {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <Container as={SafeAreaView} bg="white" flex={1}>
       {isLoading ? (
-        <ActivityIndicator color="blue" />
+        <ContainerCenter>
+          <ActivityIndicator color="blue" />
+        </ContainerCenter>
       ) : (
-        <View style={styles.container}>
-          <TextInput
-            style={styles.inputStyle}
+        <ContainerCenter>
+          <LoginLogo />
+
+          <Container alignItems="center">
+            <Text fontSize={24} fontFamily={theme.typography.bold} mt={30}>
+              Welcome
+            </Text>
+            <Text
+              fontSize={14}
+              fontFamily={theme.typography.regular}
+              mt={10}
+              style={{textTransform: 'uppercase'}}>
+              Please Login Use The App
+            </Text>
+          </Container>
+
+          <Input
+            m={14}
+            width={300}
+            color="black"
             placeholder="Email"
+            keyboardType="email-address"
             value={email}
             onChangeText={(val) => setEmail(val)}
           />
-          <TextInput
-            style={styles.inputStyle}
-            placeholder="Password"
-            value={password}
-            onChangeText={(val) => setPassword(val)}
+
+          <PasswordInput
+            placeHolder={'Password'}
+            password={password}
+            icon={icon}
+            passwordToggle={passwordToggle}
+            setPassword={setPassword}
+            togglePassword={togglePassword}
           />
-          <Button color="blue" title="Login" onPress={() => userLogin()} />
+
+          <Container mt={10} mb={20}>
+            <Text
+              fontSize={theme.smallSize}
+              color="blue"
+              ml={150}
+              onPress={() => navigation.navigate('ForgotPassword')}>
+              Forgot Password ?
+            </Text>
+          </Container>
+
+          <ActionButton m={15} onPress={() => userLogin()}>
+            <ActionButtonTitle fontSize={theme.smallSize}>
+              Sign In
+            </ActionButtonTitle>
+          </ActionButton>
+
           <Text
-            style={styles.loginText}
+            fontSize={theme.smallSize}
+            color="blue"
+            mt={15}
             onPress={() => navigation.navigate('Register')}>
-            Already Registered? Click here to login
+            <Text color="grey">New user ?</Text> Sign Up
           </Text>
-        </View>
+        </ContainerCenter>
       )}
-    </SafeAreaView>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    padding: 35,
-    backgroundColor: '#fff',
-  },
-  inputStyle: {
-    width: '100%',
-    marginBottom: 15,
-    paddingBottom: 15,
-    alignSelf: 'center',
-    borderColor: '#ccc',
-    borderBottomWidth: 1,
-  },
-  loginText: {
-    color: '#3740FE',
-    marginTop: 25,
-    textAlign: 'center',
-  },
-  preloader: {
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-});
 
 export default LoginView;
